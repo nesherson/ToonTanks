7cda163a852efad9024a619e0569cd64596439d2
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-10-11 09:09:05 +0200

    Initial commit

---
+++ Source/ToonTanks/Private/BasePawn.cpp
@@ -0,0 +1,22 @@
+#include "BasePawn.h"
+
+ABasePawn::ABasePawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABasePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/ToonTanks/Public/BasePawn.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API ABasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABasePawn();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
